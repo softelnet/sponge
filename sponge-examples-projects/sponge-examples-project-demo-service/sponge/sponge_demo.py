ed91da02801c2fe7e485141c47178f0a2e962b7f
Marcin Pas <marcin.pas@softelnet.com>
2018-09-07 14:09:53 +0200

    Minor changes in the Demo service.

--- sponge-examples-projects/sponge-examples-project-demo-service/sponge/sponge_demo.py
+++ sponge-examples-projects/sponge-examples-project-demo-service/sponge/sponge_demo.py
@@ -2,3 +2,15 @@
 Sponge Knowledge base
 Demo
 """
+
+class DrawDoodle(Action):
+    def onConfigure(self):
+        self.displayName = "Draw a doodle"
+        self.description = "Shows a canvas to draw a doodle"
+        self.argsMeta = [ArgMeta("image", BinaryType().mimeType("image/png")
+                   .features({"source":"drawing", "width":200, "height":250, "background":"FFFFFF", "color":"000000", "strokeWidth":5}))\
+                   .displayName("Doodle")
+        ]
+        self.resultMeta = ResultMeta(StringType()).displayName("Result")
+    def onCall(self, image):
+        return "OK"
