3db686095fb3a8370d97a3a099d5ba29f406dbcb
Marcin Pas <marcin.pas@softelnet.com>
2020-04-02 00:27:52 +0200

    Minor changes in the examples.

---
+++ sponge-examples-projects/sponge-examples-project-demo-service/sponge/sponge_demo_map_type.py
@@ -0,0 +1,27 @@
+"""
+Sponge Knowledge base
+Demo - A map type
+"""
+
+class ActionWithMapTypeArgs(Action):
+    def onConfigure(self):
+        self.withLabel("Map type arguments")
+        self.withArg(StringType("data").withLabel("Map data").withProvided(ProvidedMeta().withValueSet())).withResult(
+            MapType().withLabel("Map")
+                .withKey(IntegerType("key").withLabel("Key"))
+                .withValue(StringType("value").withLabel("Value")))
+    def onCall(self, data):
+        if data == "data":
+            return {1:"One", 2:"Two", 3:"Three", 4:"Four", 5:"Five"}
+        elif data == 'noData':
+            return {}
+        elif data == 'null':
+            return None
+        return data
+    def onProvideArgs(self, context):
+        if "data" in context.provide:
+            context.provided["data"] = ProvidedValue().withAnnotatedValueSet([
+                AnnotatedValue("data").withValueLabel("Map with data"),
+                AnnotatedValue("noData").withValueLabel("Map with no data"),
+                AnnotatedValue("null").withValueLabel("Map with a null value")
+            ])
--- sponge-tensorflow/examples/tensorflow/digits/actions/digits_predict_probabilities.py
+++ sponge-tensorflow/examples/tensorflow/digits/actions/digits_predict_probabilities.py
@@ -6,7 +6,8 @@ Digits recognition library
 class DigitsPredictProbabilities(Action):
     def onConfigure(self):
         self.withLabel("Recognize a digit (probabilities)").withDescription("Recognizes a handwritten digit returning probabilities")
-        self.withArg(createImageType("image")).withResult(MapType(StringType(), NumberType()).withLabel("Digit probabilities"))
+        self.withArg(createImageType("image")).withResult(MapType(StringType("digit").withLabel("Digit"), NumberType("probability").withLabel("Probability"))
+                                                          .withLabel("Digit probabilities"))
         self.withFeature("icon", "brain")
     def onCall(self, image):
         return py4j.facade.predict(image)
