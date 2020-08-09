f44464ded211ebed6bc9d4563eb2b723afefc946
Marcin Pas <marcin.pas@softelnet.com>
2020-08-09 19:10:31 +0200

    Minor changes in the examples.

--- sponge-app/sponge-app-demo-service/sponge/sponge_demo_submittable_action_arg.py
+++ sponge-app/sponge-app-demo-service/sponge/sponge_demo_submittable_action_arg.py
@@ -20,17 +20,17 @@ class SubmittableActionArg(Action):
                 ProvidedMeta().withValue().withDependency("arg1").withOptionalMode()),
         ]).withNonCallable()
         self.withFeatures({"cancelLabel":"Close"})
-    def onInit(self):
-        self.arg1value = "a"
-    def onProvideArgs(self, context):
-        if "arg1" in context.submit:
-            self.arg1value = context.current["arg1"]
 
+    def onProvideArgs(self, context):
         if "arg1" in context.provide:
-            context.provided["arg1"] = ProvidedValue().withValue(self.arg1value)
+            context.provided["arg1"] = ProvidedValue().withValue("a")
+
+        # Current arg1 is set when it is submitted or any of arg2, arg3 or arg4 is to be provided.
+        arg1 = context.current.get("arg1")
+
         if "arg2" in context.provide or "arg1" in context.submit:
-            context.provided["arg2"] = ProvidedValue().withValue(self.arg1value.upper() if self.arg1value else None)
+            context.provided["arg2"] = ProvidedValue().withValue(arg1.upper() if arg1 else None)
         if "arg3" in context.provide or "arg1" in context.submit:
-            context.provided["arg3"] = ProvidedValue().withValue(self.arg1value.upper() if self.arg1value else None)
+            context.provided["arg3"] = ProvidedValue().withValue(arg1.upper() if arg1 else None)
         if "arg4" in context.provide or "arg1" in context.submit:
-            context.provided["arg4"] = ProvidedValue().withValue(self.arg1value.upper() if self.arg1value else None)
+            context.provided["arg4"] = ProvidedValue().withValue(arg1.upper() if arg1 else None)
