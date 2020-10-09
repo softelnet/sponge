5a6c8d231946e02eaea67611f0f832a218861960
Marcin Pas <marcin.pas@softelnet.com>
2020-10-09 12:07:11 +0200

    Added a new article to the blog. Refactoring of mpd-mpc kb.

--- sponge-kb/sponge-kb-mpd-mpc/src/main/resources/sponge/mpd-mpc/mpd_mpc_player.py
+++ sponge-kb/sponge-kb-mpd-mpc/src/main/resources/sponge/mpd-mpc/mpd_mpc_player.py
@@ -3,44 +3,55 @@ Sponge Knowledge Base
 MPD player.
 """
 
-import os
-
 class MpdPlayer(Action):
     def onConfigure(self):
         self.withLabel("Player").withDescription("The MPD player.")
         self.withArgs([
-            StringType("song").withLabel("Song").withNullable().withReadOnly().withFeatures({"multiline":True, "maxLines":2}).withProvided(
-                ProvidedMeta().withValue()),
-            StringType("album").withLabel("Album").withNullable().withReadOnly().withFeatures({"multiline":True, "maxLines":2}).withProvided(
-                ProvidedMeta().withValue()),
-            StringType("date").withLabel("Date").withNullable().withReadOnly().withProvided(
-                ProvidedMeta().withValue()),
-            IntegerType("position").withLabel("Position").withNullable().withAnnotated().withMinValue(0).withMaxValue(100).withFeatures(
-                {"widget":"slider", "group":"position"}).withProvided(
-                ProvidedMeta().withValue().withOverwrite().withSubmittable()),
-            StringType("time").withLabel("Time").withNullable().withReadOnly().withFeatures({"group":"position"}).withProvided(
-                ProvidedMeta().withValue()),
+            # The song info arguments.
+            StringType("song").withLabel("Song").withNullable().withReadOnly()
+                .withProvided(ProvidedMeta().withValue()),
+            StringType("album").withLabel("Album").withNullable().withReadOnly()
+                .withProvided(ProvidedMeta().withValue()),
+            StringType("date").withLabel("Date").withNullable().withReadOnly()
+                .withProvided(ProvidedMeta().withValue()),
 
+            # The position arguments.
+            IntegerType("position").withLabel("Position").withNullable().withAnnotated()
+                .withMinValue(0).withMaxValue(100)
+                .withFeatures({"widget":"slider", "group":"position"})
+                .withProvided(ProvidedMeta().withValue().withOverwrite().withSubmittable()),
+            StringType("time").withLabel("Time").withNullable().withReadOnly()
+                .withFeatures({"group":"position"}).withProvided(ProvidedMeta().withValue()),
 
-            VoidType("prev").withLabel("Previous").withAnnotated().withFeatures({
-                "icon":IconInfo().withName("skip-previous").withSize(30), "group":"navigation", "align":"center"}).withProvided(
-                ProvidedMeta().withValue().withOverwrite().withSubmittable()),
-            BooleanType("play").withLabel("Play").withAnnotated().withFeatures({"group":"navigation"}).withProvided(
-                ProvidedMeta().withValue().withOverwrite().withSubmittable().withLazyUpdate()),
-            VoidType("next").withLabel("Next").withAnnotated().withFeatures({"icon":IconInfo().withName("skip-next").withSize(30), "group":"navigation"}).withProvided(
-                ProvidedMeta().withValue().withOverwrite().withSubmittable()),
+            # The navigation arguments.
+            VoidType("prev").withLabel("Previous").withAnnotated()
+                .withFeatures({"icon":IconInfo().withName("skip-previous").withSize(30),
+                               "group":"navigation", "align":"center"})
+                .withProvided(ProvidedMeta().withValue().withOverwrite().withSubmittable()),
+            BooleanType("play").withLabel("Play").withAnnotated().withFeatures({"group":"navigation"})
+                .withProvided(ProvidedMeta().withValue().withOverwrite().withSubmittable().withLazyUpdate()),
+            VoidType("next").withLabel("Next").withAnnotated()
+                .withFeatures({"icon":IconInfo().withName("skip-next").withSize(30), "group":"navigation"})
+                .withProvided(ProvidedMeta().withValue().withOverwrite().withSubmittable()),
 
-            IntegerType("volume").withLabel("Volume").withAnnotated().withMinValue(0).withMaxValue(100).withFeatures({"widget":"slider"}).withProvided(
-                ProvidedMeta().withValue().withOverwrite().withSubmittable().withLazyUpdate()),
+            # The volume argument.
+            IntegerType("volume").withLabel("Volume").withAnnotated().withMinValue(0).withMaxValue(100)
+                .withFeatures({"widget":"slider"})
+                .withProvided(ProvidedMeta().withValue().withOverwrite().withSubmittable().withLazyUpdate()),
 
-            BooleanType("repeat").withLabel("Repeat").withAnnotated().withFeatures({"group":"mode", "widget":"toggleButton", "icon":"repeat", "align":"right"}).withProvided(
-                ProvidedMeta().withValue().withOverwrite().withSubmittable().withLazyUpdate()),
-            BooleanType("single").withLabel("Single").withAnnotated().withFeatures({"group":"mode", "widget":"toggleButton", "icon":"numeric-1"}).withProvided(
-                ProvidedMeta().withValue().withOverwrite().withSubmittable().withLazyUpdate()),
-            BooleanType("random").withLabel("Random").withAnnotated().withFeatures({"group":"mode", "widget":"toggleButton", "icon":"mixer"}).withProvided(
-                ProvidedMeta().withValue().withOverwrite().withSubmittable().withLazyUpdate()),
-            BooleanType("consume").withLabel("Consume").withAnnotated().withFeatures({"group":"mode", "widget":"toggleButton", "icon":"pac-man"}).withProvided(
-                ProvidedMeta().withValue().withOverwrite().withSubmittable().withLazyUpdate())
+            # The mode arguments.
+            BooleanType("repeat").withLabel("Repeat").withAnnotated()
+                .withFeatures({"group":"mode", "widget":"toggleButton", "icon":"repeat", "align":"right"})
+                .withProvided(ProvidedMeta().withValue().withOverwrite().withSubmittable().withLazyUpdate()),
+            BooleanType("single").withLabel("Single").withAnnotated()
+                .withFeatures({"group":"mode", "widget":"toggleButton", "icon":"numeric-1"})
+                .withProvided(ProvidedMeta().withValue().withOverwrite().withSubmittable().withLazyUpdate()),
+            BooleanType("random").withLabel("Random").withAnnotated()
+                .withFeatures({"group":"mode", "widget":"toggleButton", "icon":"shuffle"})
+                .withProvided(ProvidedMeta().withValue().withOverwrite().withSubmittable().withLazyUpdate()),
+            BooleanType("consume").withLabel("Consume").withAnnotated()
+                .withFeatures({"group":"mode", "widget":"toggleButton", "icon":"pac-man"})
+                .withProvided(ProvidedMeta().withValue().withOverwrite().withSubmittable().withLazyUpdate())
         ]).withNonCallable().withActivatable()
         self.withFeatures({"refreshEvents":["statusPolling", "mpdNotification_.*"], "icon":"music", "contextActions":[
             SubAction("MpdPlaylist"),
@@ -54,88 +65,12 @@ class MpdPlayer(Action):
     def onIsActive(self, context):
         return sponge.getVariable("mpc").isConnected()
 
-    def __ensureStatus(self, mpc, status):
-        return status if mpc.isStatusOk(status) else mpc.getStatus()
-
     def onProvideArgs(self, context):
-        mpc = sponge.getVariable("mpc")
-        status = None
-        (position, size) = (None, None)
+        """This callback method:
+        a) Modifies the MPD state by using the argument values submitted by the user. The names
+        are specified in the context.submit set, the values are stored in the context.current map.
+        b) Sets the values of arguments that are to be provided to the client. The names are
+        specified in the context.provide set, the values are to be stored in the context.provided map.
+        """
+        MpdPlayerProvideArgsRuntime(context).run()
 
-        mpc.lock.lock()
-        try:
-            try:
-                if "position" in context.submit:
-                    if context.current["position"]:
-                        status = mpc.seekByPercentage(context.current["position"].value)
-                if "volume" in context.submit:
-                        status = mpc.setVolume(context.current["volume"].value)
-                if "play" in context.submit:
-                    status = mpc.togglePlay(context.current["play"].value)
-                if "prev" in context.submit:
-                    status = mpc.prev()
-                if "next" in context.submit:
-                    status = mpc.next()
-
-                if "repeat" in context.submit:
-                    status = mpc.setMode("repeat", context.current["repeat"].value)
-                if "single" in context.submit:
-                    status = mpc.setMode("single", context.current["single"].value)
-                if "random" in context.submit:
-                    status = mpc.setMode("random", context.current["random"].value)
-                if "consume" in context.submit:
-                    status = mpc.setMode("consume", context.current["consume"].value)
-            except:
-                sponge.logger.warn("Submit error: {}", sys.exc_info()[1])
-
-            currentSong = None
-            if "song" in context.provide or "date" in context.provide:
-                currentSong = mpc.getCurrentSong()
-            if "song" in context.provide:
-                context.provided["song"] = ProvidedValue().withValue(mpc.getSongLabel(currentSong) if currentSong else None)
-            if "album" in context.provide:
-                context.provided["album"] = ProvidedValue().withValue(currentSong["album"] if currentSong else None)
-            if "date" in context.provide:
-                context.provided["date"] = ProvidedValue().withValue(currentSong["date"] if currentSong else None)
-            if "position" in context.provide or "context" in context.submit:
-                status = self.__ensureStatus(mpc, status)
-                context.provided["position"] = ProvidedValue().withValue(AnnotatedValue(mpc.getPositionByPercentage(status)).withFeature(
-                    "enabled", mpc.isStatusPlayingOrPaused(status)))
-            if "time" in context.provide:
-                status = self.__ensureStatus(mpc, status)
-                context.provided["time"] = ProvidedValue().withValue(mpc.getTimeStatus(status))
-            # Provide an annotated volume value at once if submitted.
-            if "volume" in context.provide or "volume" in context.submit:
-                status = self.__ensureStatus(mpc, status)
-                volume = mpc.getVolume(status)
-                context.provided["volume"] = ProvidedValue().withValue(AnnotatedValue(volume).withTypeLabel(
-                    "Volume" + ((" (" + str(volume) + "%)") if volume else "")))
-            if "play" in context.provide:
-                status = self.__ensureStatus(mpc, status)
-                playing = mpc.getPlay(status)
-                context.provided["play"] = ProvidedValue().withValue(AnnotatedValue(playing).withFeature("icon",
-                        IconInfo().withName("pause" if playing else "play").withSize(60)))
-
-            if "prev" in context.provide or "next" in context.provide:
-                status = self.__ensureStatus(mpc, status)
-                (position, size) = mpc.getCurrentPlaylistPositionAndSize(status)
-            if "prev" in context.provide:
-                context.provided["prev"] = ProvidedValue().withValue(AnnotatedValue(None).withFeature("enabled", position is not None))
-            if "next" in context.provide:
-                context.provided["next"] = ProvidedValue().withValue(AnnotatedValue(None).withFeature("enabled",
-                        position is not None and size is not None))
-
-            if "repeat" in context.provide or "single" in context.provide or "random" in context.provide or "consume" in context.provide:
-                status = self.__ensureStatus(mpc, status)
-                modes = mpc.getModes(status)
-
-                if "repeat" in context.provide:
-                    context.provided["repeat"] = ProvidedValue().withValue(AnnotatedValue(modes.get("repeat", False)))
-                if "single" in context.provide:
-                    context.provided["single"] = ProvidedValue().withValue(AnnotatedValue(modes.get("single", False)))
-                if "random" in context.provide:
-                    context.provided["random"] = ProvidedValue().withValue(AnnotatedValue(modes.get("random", False)))
-                if "consume" in context.provide:
-                    context.provided["consume"] = ProvidedValue().withValue(AnnotatedValue(modes.get("consume", False)))
-        finally:
-            mpc.lock.unlock()
---
+++ sponge-kb/sponge-kb-mpd-mpc/src/main/resources/sponge/mpd-mpc/mpd_mpc_player_runtime.py
@@ -0,0 +1,143 @@
+"""
+Sponge Knowledge Base
+MPD player runtime.
+"""
+
+class MpdPlayerProvideArgsRuntime:
+    def __init__(self, context):
+        self.context = context
+
+        # An instance of the Mpc class that is a wrapper for the mpc (MPD client) commandline calls.
+        self.mpc = sponge.getVariable("mpc")
+
+        # Stores and caches the latest MPD status as returned by the mpc command.
+        self.status = None
+
+        # Providers for the submittable arguments.
+        self.submitProviders = {
+            "position":lambda value: self.mpc.seekByPercentage(value),
+            "volume":lambda value: self.mpc.setVolume(value),
+
+            "play":lambda value: self.mpc.togglePlay(value),
+            "prev":lambda value: self.mpc.prev(),
+            "next":lambda value: self.mpc.next(),
+
+            "repeat":lambda value: self.mpc.setMode("repeat", value),
+            "single":lambda value: self.mpc.setMode("single", value),
+            "random":lambda value: self.mpc.setMode("random", value),
+            "consume":lambda value: self.mpc.setMode("consume", value),
+            }
+
+        # The MPD mode types (that correspond to the action arguments).
+        self.modeTypes = ["repeat", "single" , "random", "consume"]
+
+    def run(self):
+        # Enter the critical section.
+        self.mpc.lock.lock()
+        try:
+            # Pass the context as an argument to the private methods in order to simplify the code.
+
+            # Submit arguments sent from the client.
+            self.__submitArgs(self.context)
+
+            # Provide arguments to the client.
+            self.__provideInfoArgs(self.context)
+            self.__providePositionArgs(self.context)
+            self.__provideVolumeArg(self.context)
+            self.__provideNavigationArgs(self.context)
+            self.__provideModeArgs(self.context)
+        finally:
+            self.mpc.lock.unlock()
+
+    def __submitArgs(self, context):
+        """Submits arguments sent from the client by changing the state of the MPD daemon.
+        """
+        # Use simple providers to submit values.
+        for name in context.submit:
+            provider = self.submitProviders.get(name)
+            if provider:
+                try:
+                    # Cache the current MPD status.
+                    self.status = provider(context.current[name].value if context.current[name] else None)
+                except:
+                    sponge.logger.warn("Submit error: {}", sys.exc_info()[1])
+
+    def __getStatus(self):
+        # Cache an mpc command status text.
+        if not self.mpc.isStatusOk(self.status):
+            self.status = self.mpc.getStatus()
+
+        return self.status
+
+    def __provideInfoArgs(self, context):
+        """Provides the song info arguments to the client.
+        """
+        # Read the current song from the MPD daemon only if necessary.
+        currentSong = self.mpc.getCurrentSong() if any(arg in context.provide for arg in ["song", "album" , "date"]) else None
+
+        if "song" in context.provide:
+            context.provided["song"] = ProvidedValue().withValue(
+                self.mpc.getSongLabel(currentSong) if currentSong else None)
+        if "album" in context.provide:
+            context.provided["album"] = ProvidedValue().withValue(
+                currentSong["album"] if currentSong else None)
+        if "date" in context.provide:
+            context.provided["date"] = ProvidedValue().withValue(
+                currentSong["date"] if currentSong else None)
+
+    def __providePositionArgs(self, context):
+        """Provides the position arguments to the client.
+        """
+        # If submitted, provide an updated annotated position too.
+        if "position" in context.provide or "context" in context.submit:
+            context.provided["position"] = ProvidedValue().withValue(
+                AnnotatedValue(self.mpc.getPositionByPercentage(self.__getStatus()))
+                    .withFeature("enabled", self.mpc.isStatusPlayingOrPaused(self.__getStatus())))
+        if "time" in context.provide:
+            context.provided["time"] = ProvidedValue().withValue(self.mpc.getTimeStatus(self.__getStatus()))
+
+    def __provideVolumeArg(self, context):
+        """Provides the volume argument to the client.
+        """
+        # If submitted, provide an updated annotated volume too.
+        if "volume" in context.provide or "volume" in context.submit:
+            volume = self.mpc.getVolume(self.__getStatus())
+            context.provided["volume"] = ProvidedValue().withValue(
+                AnnotatedValue(volume)
+                    .withTypeLabel("Volume" + ((" (" + str(volume) + "%)") if volume else "")))
+
+    def __provideNavigationArgs(self, context):
+        """Provides the navigation arguments to the client.
+        """
+        if "play" in context.provide:
+            playing = self.mpc.getPlay(self.__getStatus())
+            context.provided["play"] = ProvidedValue().withValue(
+                AnnotatedValue(playing).withFeature("icon",
+                    IconInfo().withName("pause" if playing else "play").withSize(60)))
+
+        # Read the current playlist position and size from the MPD daemon only if necessary.
+        (position, size) = (None, None)
+        if "prev" in context.provide or "next" in context.provide:
+            (position, size) = self.mpc.getCurrentPlaylistPositionAndSize(self.__getStatus())
+
+        if "prev" in context.provide:
+            context.provided["prev"] = ProvidedValue().withValue(
+                AnnotatedValue(None).withFeature("enabled", position is not None))
+        if "next" in context.provide:
+            context.provided["next"] = ProvidedValue().withValue(
+                AnnotatedValue(None).withFeature("enabled",
+                    position is not None and size is not None))
+
+    def __provideModeArgs(self, context):
+        """Provides the mode arguments to the client.
+        """
+        currentModes = None
+
+        # Provide only required modes (i.e. specified in the context.provide set).
+        for arg in [a for a in self.modeTypes if a in context.provide]:
+            if currentModes is None:
+                # Read the current modes from the MPD daemon only if necessary.
+                currentModes = self.mpc.getModes(self.__getStatus())
+
+            context.provided[arg] = ProvidedValue().withValue(
+                    AnnotatedValue(currentModes.get(arg, False)))
